928dee2366299a5fd21bae92ce636f7c67d30f84
rushi444 <rushi44@gmail.com>
2019-07-17 13:17:14 -0700

    Creates Agents Class

--- src/main/java/com/orders/orders/model/Agents.java
+++ src/main/java/com/orders/orders/model/Agents.java
@@ -1,4 +1,20 @@
 package com.orders.orders.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="Agent")
 public class Agents {
+    private long agentcode;
+
+    private String agentname;
+    private String workingarea;
+    private double commision;
+    private String phone;
+    private String country;
+
+    private List<Customer> customers = new ArrayList<>();
 }
